6ed33a597a1fb05c00f0c62c6bce0a0afa766169
www-net <78052177+www-net@users.noreply.github.com>
2023-06-02 17:52:06 +0300

    added logic to switch slides in the personal cabinet

---
+++ js/account.js
@@ -0,0 +1,52 @@
+// Включить редактирование полей ввода
+
+const detailsItems = document.querySelectorAll('.details__item');
+// const inputsArr = document.querySelectorAll('.details__input');
+// const detailsArr = document.querySelectorAll('.details__item-button');
+
+detailsItems.forEach((elem) => {
+  const input = elem.querySelector('.details__input');
+  const button = elem.querySelector('.details__item-button');
+
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (input.hasAttribute('readonly')) {
+      input.removeAttribute('readonly');
+      input.focus();
+    } else {
+      input.setAttribute('readonly', '');
+    }
+  })
+})
+
+
+// Переключение разделов личного кабинета
+
+const servicesSlides = document.querySelectorAll(".account__slide");
+const servicesControls = document.querySelectorAll(".account__button");
+
+let servicesSlideIndex = 0;
+showServiceSlides(servicesSlideIndex);
+
+for (let i =0; i < servicesControls.length; i++ ) {
+    servicesControls[i].addEventListener("click", function () {
+        showServiceSlides(i);
+    });
+}
+
+function showServiceSlides(n) {
+	for (let i = 0; i < servicesSlides.length; i++) {
+		if (servicesSlides[i].classList.contains("account__slide--current")) {
+			servicesSlides[i].classList.remove("account__slide--current");
+		}
+	}
+
+	for (let i = 0; i < servicesControls.length; i++) {
+		if (servicesControls[i].classList.contains("account__button--current")) {
+			servicesControls[i].classList.remove("account__button--current");
+		}
+	}
+
+	servicesSlides[n].classList.add("account__slide--current");
+	servicesControls[n].classList.add("account__button--current");
+}
